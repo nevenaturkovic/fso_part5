34bdf243d2d395cc85fbe1078637100ad322afe7
Nevena Turkovic <nevenatu@gmail.com>
2022-04-07 10:52:58 +0200

    Finished 5.1: bloglist frontend, step1

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react"
 import Blog from "./components/Blog"
+// import Notification from "./components/Notification"
 import blogService from "./services/blogs"
-import loginService from './services/login'
+import loginService from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  // const [newBlog, setNewBlog] = useState("")
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
+  // const [errorMessage, setErrorMessage] = useState(null)
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -28,29 +31,42 @@ const App = () => {
     }
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  )
+  // const loginForm = () => (
+  //   <form onSubmit={handleLogin}>
+  //     <div>
+  //       username
+  //       <input
+  //         type="text"
+  //         value={username}
+  //         name="Username"
+  //         onChange={({ target }) => setUsername(target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       password
+  //       <input
+  //         type="password"
+  //         value={password}
+  //         name="Password"
+  //         onChange={({ target }) => setPassword(target.value)}
+  //       />
+  //     </div>
+  //     <button type="submit">login</button>
+  //   </form>
+  // )
+
+  // const addBlog = (event) => {
+  //   event.preventDefault()
+  //   const blogObject = {
+  //     title: newBlog,
+  //     author: ,
+  //   }
+
+  //   blogService.create(blogObject).then((returnedBlog) => {
+  //     setBlogs(blogs.concat(returnedBlog))
+  //     setNewBlog("")
+  //   })
+  // }
 
   // const blogForm = () => (
   //   <form onSubmit={addBlog}>
@@ -59,18 +75,46 @@ const App = () => {
   //       onChange={handleBlogChange}
   //     />
   //     <button type="submit">save</button>
-  //   </form>  
+  //   </form>
   // )
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {user === null && loginForm()}
-      
+      <p>{user.name} logged-in</p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
