3276e80fa7c5f2b376dc1be5644b3f6e34353662
Nevena Turkovic <nevenatu@gmail.com>
2022-04-07 08:13:45 +0200

    If user is not logged in then login form is shown.

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,45 +1,76 @@
 import { useState, useEffect } from "react"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch (exception) {
+      // setErrorMessage("Wrong credentials")
+      // setTimeout(() => {
+      //   setErrorMessage(null)
+      // }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  // const blogForm = () => (
+  //   <form onSubmit={addBlog}>
+  //     <input
+  //       value={newBlog}
+  //       onChange={handleBlogChange}
+  //     />
+  //     <button type="submit">save</button>
+  //   </form>  
+  // )
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
-  const handleLogin = (event) => {
-    event.preventDefault()
-    console.log("logging in with", username, password)
-  }
-
   return (
     <div>
       <h2>blogs</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      {user === null && loginForm()}
+      
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
