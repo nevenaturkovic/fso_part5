f4187aed3063a437c2439788b389f3b9b0799aa1
Nevena Turkovic <nevenatu@gmail.com>
2022-04-10 22:44:39 +0200

    Implemented logout button.

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -63,7 +63,18 @@ const App = () => {
   const blogList = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button
+          onClick={() => {
+            window.localStorage.removeItem("loggedBlogappUser")
+            blogService.setToken(null)
+            setUser(null)
+          }}
+        >
+          logout
+        </button>
+      </p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
